03ab218d98ba5823ee4b48d4ce285c9cd8e3d591
abhishek malvadkar ranoji <abhishek@dev.com>
2022-08-06 22:10:43 +0530

    Createc product entity class

--- product-management-system/src/main/java/com/pms/entityies/ProductEntity.java
+++ product-management-system/src/main/java/com/pms/entityies/ProductEntity.java
@@ -5,27 +5,76 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "product_details")
+public class ProductEntity {
+	@Id
+	@Column(name = "Id")
+	private Integer id;
 
-public class PriductEntity {
-	 @Id
-	 @Column(name="Id")
-	 private Integer product_id;
-	 
-	 @Column(name="PRODUCT_PRICE")
-	private String product_name;
-	 
-	 @Column(name="PRODUCT_PRICE")
-	private Integer product_price;
-	 
-	 @Column(name="PRODUCT_STATUS")
+	@Column(name = "PRODUCT_NAME")
+	private String name;
+
+	@Column(name = "PRODUCT_PRICE")
+	private Integer price;
+
+	@Column(name = "PRODUCT_STATUS")
 	private Boolean isAvailable;
-	 @Column (name="CREATED_ON")
-	 private  final static LocalDate createdOn = LocalDate.now();
-	 
-	 @Column(name="UPDATED_ON")
-	 private final static LocalDate updatedOn = LocalDate.now();
-	 
+
+	@Column(name = "CREATED_ON")
+	private LocalDate createdOn;
+
+	@Column(name = "UPDATED_ON")
+	private LocalDate updatedOn;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Boolean getIsAvailable() {
+		return isAvailable;
+	}
+
+	public void setIsAvailable(Boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public LocalDate getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(LocalDate createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public LocalDate getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(LocalDate updatedOn) {
+		this.updatedOn = updatedOn;
+	}
 
 }
